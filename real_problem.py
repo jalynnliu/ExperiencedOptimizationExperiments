0caacc02b072ccffdd60473f7051f173a7443bf4
jalynn <jalynnlau@outlook.com>
2019-08-19 17:46:49 +0800

    add average method

--- real_problem.py
+++ real_problem.py
@@ -12,7 +12,7 @@ from ExpLearn import ImageNet
 from ParamsHelper import ParamsHelper
 import lightgbm as lgb
 from sklearn.metrics import f1_score
-from sklearn import preprocessing
+from Tools import BenchmarkHelper
 import pandas as pd
 import os
 
@@ -96,23 +96,22 @@ def run_for_synthetic_problem():
     log_buffer.append('advance num: ' + str(adv_threshold))
     log_buffer.append('+++++++++++++++++++++++++++++++')
 
-    predictors, nets = get_predicotrs()
     expert = Experts(predictors=predictors, eta=eta)
     test_10 = "adult,balance-scale,cnae,credit-g,crx,cylinder,flare,solar-flare_2,german".split(',')
 
-    opt_error_list = []
+
+    mlbp=BenchmarkHelper()
 
     for problem_name in test_10:
-        train_file = path + '/cache/' + problem_name + '/train.csv'
-        test_file = path + '/cache/' + problem_name + '/test.csv'
-        dtrain = pd.read_csv(train_file)
-        dtest = pd.read_csv(test_file)
-        dtrain=data_process(dtrain)
+        dtrain,dtest = mlbp.get_train_test_data(problem_name)
+        opt_error_list = []
 
         print('optimize ', problem_name, '===================================================')
         log_buffer.append('optimize ' + problem_name + '===================================================')
 
         for j in range(opt_repeat):
+            print('============================ ', j, '================================')
+            log_buffer.append('============================ ' + str(j) + '=================================')
 
             exp_racos = ExpAdaRacosOptimization(dimension, expert)
             exp_racos.set_parameters(ss=sample_size, bud=budget, pn=positive_num, rp=rand_probability, ub=uncertain_bit,
@@ -121,9 +120,7 @@ def run_for_synthetic_problem():
             start_t = time.time()
             x = exp_racos.sample()
             for i in range(budget):
-                if i % 10 == 0:
-                    # print '======================================================'
-                    print('budget ', i, ':', x.get_fitness())
+
                 hyper_param = (sample_codec.sample_decode(x))
                 model = lgb.LGBMClassifier()
                 model.set_params(**hyper_param)
@@ -132,6 +129,11 @@ def run_for_synthetic_problem():
                 fitness = -f1_score(dtest[:, -1], pred, average='macro')
                 exp_racos.update_model(x, fitness)
                 x = exp_racos.sample()
+                if i % 10 == 0 and i>sample_size+positive_num:
+                    opt=exp_racos.get_optimal()
+                    # print '======================================================'
+                    print('budget ', i, ':', opt.get_fitness())
+                    log_buffer.append('budget '+str(i)+ ':'+ str(opt.get_fitness()))
 
             end_t = time.time()
 
@@ -159,7 +161,7 @@ def run_for_synthetic_problem():
         log_buffer.append('--------------------------------------------------')
         log_buffer.append('optimization result: ' + str(opt_mean) + '#' + str(opt_std))
 
-    result_path = '/home/amax/Desktop/Results/RealProbs/'
+    result_path = '/home/amax/Desktop/ExpAdaptation/ExpAdaptation/Results/RealProbs/'
 
     optimization_log_file = result_path + 'opt-log-ada-0.txt'
     print('optimization logging: ', optimization_log_file)
@@ -168,17 +170,114 @@ def run_for_synthetic_problem():
     return
 
 
+def run_for_real_problem_ave():
+    sample_size = 10  # the instance number of sampling in an iteration
+    budget = 50  # budget in online style
+    positive_num = 2  # the set size of PosPop
+    rand_probability = 0.99  # the probability of sample in model
+    uncertain_bit = 1  # the dimension size that is sampled randomly
+    adv_threshold = 10  # advance sample size
+
+    opt_repeat = 10
+
+    eta = 0.9
+
+    hyper_space = get_hyper_space()
+    dimension, sample_codec = get_dimension(hyper_space)
+
+    log_buffer = []
+
+    log_buffer.append('+++++++++++++++++++++++++++++++')
+    log_buffer.append('optimization parameters')
+    log_buffer.append('sample size: ' + str(sample_size))
+    log_buffer.append('budget: ' + str(budget))
+    log_buffer.append('positive num: ' + str(positive_num))
+    log_buffer.append('random probability: ' + str(rand_probability))
+    log_buffer.append('uncertain bits: ' + str(uncertain_bit))
+    log_buffer.append('advance num: ' + str(adv_threshold))
+    log_buffer.append('+++++++++++++++++++++++++++++++')
+
+    test_10 = "adult,balance-scale,cnae,credit-g,crx,cylinder,flare,solar-flare_2,german".split(',')
+
+
+    mlbp=BenchmarkHelper()
+
+    for problem_name in test_10:
+        dtrain,dtest = mlbp.get_train_test_data(problem_name)
+        opt_error_list = []
+
+        print('optimize ', problem_name, '===================================================')
+        log_buffer.append('optimize ' + problem_name + '===================================================')
+
+        for j in range(opt_repeat):
+            print('============================ ', j, '================================')
+            log_buffer.append('============================ ' + str(j) + '=================================')
+
+            exp_racos = ExpRacosOptimization(dimension, nets)
+            exp_racos.clear()
+            start_t = time.time()
+            model = lgb.LGBMClassifier()
+
+            def score_fun(x):
+                    ## here is the score function
+                    hyper_param = (sample_codec.sample_decode(x))
+                    model.set_params(**hyper_param)
+                    bst = model.fit(dtrain[:, :-1], dtrain[:, -1])
+                    pred = bst.predict(dtest[:, :-1])
+                    fitness = -f1_score(dtest[:, -1], pred, average='macro')
+                    return fitness
+
+
+            exp_racos.exp_mix_opt(obj_fct=score_fun,ss=sample_size, bud=budget, pn=positive_num, rp=rand_probability, ub=uncertain_bit,
+                                     at=adv_threshold)
+
+            end_t = time.time()
+
+            print('total budget is ', budget)
+            log_buffer.append('total budget is ' + str(budget))
+
+            hour, minute, second = time_formulate(start_t, end_t)
+            print('spending time: ', hour, ':', minute, ':', second)
+            log_buffer.append('spending time: ' + str(hour) + '+' + str(minute) + '+' + str(second))
+
+            optimal = exp_racos.get_optimal()
+            opt_error = optimal.get_fitness()
+            optimal_x = optimal.get_features()
+
+            opt_error_list.append(opt_error)
+            print('validation optimal value: ', opt_error)
+            log_buffer.append('validation optimal value: ' + str(opt_error))
+            print('optimal x: ', optimal_x)
+            log_buffer.append('optimal nn structure: ' + list2string(optimal_x))
+
+        opt_mean = np.mean(np.array(opt_error_list))
+        opt_std = np.std(np.array(opt_error_list))
+        print('--------------------------------------------------')
+        print('optimization result: ', opt_mean, '#', opt_std)
+        log_buffer.append('--------------------------------------------------')
+        log_buffer.append('optimization result: ' + str(opt_mean) + '#' + str(opt_std))
+
+    result_path = '/home/amax/Desktop/ExpAdaptation/ExpAdaptation/Results/RealProbs/'
+
+    optimization_log_file = result_path + 'opt-log-ave-0.txt'
+    print('optimization logging: ', optimization_log_file)
+    fo.FileWriter(optimization_log_file, log_buffer, style='w')
+
+    return
+
+
+
 def get_hyper_space():
     hyper_space = {
         'boosting_type': ('str', ('gbdt', 'rf', 'dart')),
         'learning_rate': ('float', (1e-6, 0.1)),
         'n_estimators': ('int', (50, 100)),
         'num_leaves': ('int', (2, 1000)),
-        'colsample_bytree': ('float', (0, 1)),
-        'subsample': ('float', (0, 1)),
+        'colsample_bytree': ('float', (0.1, 1)),
+        'subsample': ('float', (0.1, 1)),
         'subsample_freq': ('int', (1, 100)),
-        'reg_alpha': ('float', (0, 1)),
-        'reg_lambda': ('float', (0, 1)),
+        'reg_alpha': ('float', (0.1, 1)),
+        'reg_lambda': ('float', (0.1, 1)),
         'min_child_weight': ('float', (0, 0.01)),
         'min_child_samples': ('int', (10, 30)),
     }
@@ -203,4 +302,7 @@ def get_dimension(param_input):
 
 
 if __name__ == '__main__':
+    predictors, nets = get_predicotrs()
+    run_for_real_problem_ave()
     run_for_synthetic_problem()
+
